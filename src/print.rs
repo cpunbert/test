03d236b8d35c8514f48cc8d1db058e80f179fafa
cpunbert <hubert.lisica@gmail.com>
2022-04-07 20:36:11 +0200

    asd

---
+++ src/print.rs
@@ -0,0 +1,13 @@
+pub fn run(){
+    //Print to console 
+    println!("Hello World");
+
+
+
+    //Formating
+    println!("{} is from {}","dis", "fordon");
+
+    //Position arguments
+    println!("{1} is from {1} and {0} likes to {2}", "Brad", "Mass", "code")
+
+}
